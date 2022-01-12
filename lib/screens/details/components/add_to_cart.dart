368999f81ebf3c9200ab4377341862deb4afa7ab
Dinfinity8 <dsawsome@gmail.com>
2022-01-11 22:51:43 -0500

    Implemented session variable and badge on cart.

--- lib/screens/details/components/add_to_cart.dart
+++ lib/screens/details/components/add_to_cart.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/models/Product.dart';
+import 'package:shop_app/models/ProductOrder.dart';
+import 'package:shop_app/models/SessionStorageHelper.dart';
+import 'package:shop_app/screens/home/components/shop_cart.dart';
 
 import '../../../constants.dart';
 
@@ -33,17 +36,41 @@ class AddToCart extends StatelessWidget {
                 "assets/icons/add_to_cart.svg",
                 color: product.color,
               ),
-              onPressed: () {},
+              onPressed: () {
+                /// Retrive current product orders from session
+                List<ProductOrder> cartList =
+                    SessionStorageHelper.getProductOrders('cartList');
+
+                /// Add the new product order
+                cartList.add(ProductOrder(
+                    productId: product.id,
+                    count: SessionStorageHelper.getProductCount()));
+
+                /// Saving the new product order into the session
+                SessionStorageHelper.saveValue('cartList', cartList);
+
+                /// Rebuild Shop Cart
+                print('***add_to_cart1: call buildShopCart()');
+                buildShopCart();
+                print('***add_to_cart2: called buildShopCart()');
+              },
             ),
           ),
           Expanded(
             child: SizedBox(
               height: 50,
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                color: product.color,
-                onPressed: () {},
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(
+                    color: product.color,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                ),
+                onPressed: () {
+                  // TODO: Implement Add to Cart function
+                  print("***Add to Cart Pressed!");
+                },
                 child: Text(
                   "Buy  Now".toUpperCase(),
                   style: TextStyle(
